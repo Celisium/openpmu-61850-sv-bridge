78395d23028f4bc01d25f57a0b7391e4dda17729
IDidMakeThat <ididmakethat@outlook.com>
2024-11-01 14:12:04 +0000

    Receive timestamp from socket
    The sample timestamp is now obtained from the socket using SO_TIMESTAMPNS
    rather than getting the time after reading the socket. This probably won't
    matter too much in practice but will provide a slightly more accurate time.
    
    I also snuck in a command line option to choose the network interface, which I
    included in this commit because I'm bad at Git.

--- mu_to_openpmu/__main__.py
+++ mu_to_openpmu/__main__.py
@@ -1,6 +1,7 @@
+import argparse
 import logging
 import socket
-import time
+import struct
 
 from .bytes_reader import BytesReader
 from .packet import read_sv
@@ -11,23 +12,37 @@ ETHERTYPE_SV = 0x88BA
 MAX_PACKET_LENGTH = 1522
 SAMPLE_LENGTH = 32
 
+# Python does not provide this definition in the socket module.
+SO_TIMESTAMPNS_NEW = 64
+
 
 def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--interface")
+    args = parser.parse_args()
+
+    interface = args.interface or "lo"
+
     logger = logging.getLogger()
     logging.basicConfig(level=logging.DEBUG)
 
     with open("data_out.bin", "wb") as out_file, open("data_out.xml", "wt") as xml_file:
         with socket.socket(socket.AF_PACKET, socket.SOCK_DGRAM, ETHERTYPE_SV) as skt:
-            skt.bind((INTERFACE_NAME, ETHERTYPE_SV))
+            skt.bind((interface, ETHERTYPE_SV))
 
-            logger.info("Successfully bound socket to interface '%s'", INTERFACE_NAME)
+            skt.setsockopt(socket.SOL_SOCKET, SO_TIMESTAMPNS_NEW, 1)
+
+            logger.info("Successfully bound socket to interface '%s'", interface)
 
             sample_buffer = SampleBufferManager(80 * 60, out_file, xml_file)
 
             while True:
-                (msg, address) = skt.recvfrom(MAX_PACKET_LENGTH)
-                # TODO: Timestamp should be obtained from socket using SO_TIMESTAMP or similar.
-                sample_recv_time = time.time_ns()
+                (msg, anc_data, msg_flags, address) = skt.recvmsg(
+                    MAX_PACKET_LENGTH, socket.CMSG_SPACE(16)
+                )
+                (_, _, cmsg_data) = anc_data[0]
+                (tv_sec, tv_nsec) = struct.unpack("=qq", cmsg_data)
+                sample_recv_time_ns = tv_sec * 1000000000 + tv_nsec
 
                 reader = BytesReader(msg)
                 _appid = reader.read_u16_be()
@@ -39,7 +54,7 @@ def main() -> None:
                     savpdu = read_sv(reader.sub_reader(length))
 
                     for asdu in savpdu.asdus:
-                        sample_buffer.add_sample(sample_recv_time, asdu)
+                        sample_buffer.add_sample(sample_recv_time_ns, asdu)
 
                 except RuntimeError as err:
                     with open("debug_dump.bin", "wb") as file:
